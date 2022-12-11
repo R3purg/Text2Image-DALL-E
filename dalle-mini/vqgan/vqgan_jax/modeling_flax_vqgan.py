61afad2142373ae07c07f9900be0986ddbab0f0c
R3purg <marcius989@gmail.com>
2022-12-11 16:27:38 +0900

    Cutout implementation

--- dalle-mini/vqgan/vqgan_jax/modeling_flax_vqgan.py
+++ dalle-mini/vqgan/vqgan_jax/modeling_flax_vqgan.py
@@ -12,6 +12,9 @@ from flax.core.frozen_dict import FrozenDict
 
 from transformers.modeling_flax_utils import FlaxPreTrainedModel
 
+import torch
+from CLIP import clip
+import kornia.augmentation as K
 from configuration_vqgan import VQGANConfig
 
 import pytorch_lightning as pl
@@ -50,6 +53,21 @@ class Downsample(nn.Module):
 	with_conv: bool
 	dtype: jnp.dtype = jnp.float32
 
+	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+	perceptor = clip.load(
+		# args.clip_model,
+		'ViT-B/32',
+		jit=False
+	)[0].eval().requires_grad_(False).to(device)
+	cut_size = perceptor.visual.input_resolution
+	make_cutouts = nn.MakeCutouts(
+		cut_size,
+		# args.cutn,
+		32,
+		# cut_pow=args.cut_pow
+		1.
+	)
+
 	def setup(self):
 		if self.with_conv:
 			self.conv = nn.Conv(
@@ -72,6 +90,99 @@ class Downsample(nn.Module):
 																	padding="VALID")
 		return hidden_states
 
+class MakeCutouts(nn.Module):
+	cut_size: int
+	cutn: int
+	cut_pow: jnp.dtype = jnp.float32
+
+	def setup(self):
+		self.cut_size
+		self.cutn
+		self.cut_pow
+
+		self.augs = nn.Sequential(
+			# K.RandomHorizontalFlip(p=0.5),
+			# K.RandomVerticalFlip(p=0.5),
+			# K.RandomSolarize(0.01, 0.01, p=0.7),
+			# K.RandomSharpness(0.3,p=0.4),
+			# K.RandomResizedCrop(size=(self.cut_size,self.cut_size), scale=(0.1,1),  ratio=(0.75,1.333), cropping_mode='resample', p=0.5),
+			# K.RandomCrop(size=(self.cut_size,self.cut_size), p=0.5),
+			K.RandomAffine(degrees=15, translate=0.1, p=0.7, padding_mode='border'),
+			K.RandomPerspective(0.7,p=0.7),
+			K.ColorJitter(hue=0.1, saturation=0.1, p=0.7),
+			K.RandomErasing((.1, .4), (.3, 1/.3), same_on_batch=True, p=0.7),
+		)
+		self.noise_fac = 0.1
+		self.av_pool = nn.AdaptiveAvgPool2d((self.cut_size, self.cut_size))
+		self.max_pool = nn.AdaptiveMaxPool2d((self.cut_size, self.cut_size))
+
+	def __call__(self, input):
+		# sideY, sideX = input.shape[2:4]
+		# max_size = min(sideX, sideY)
+		# min_size = min(sideX, sideY, self.cut_size)
+		cutouts = []
+
+		for _ in range(self.cutn):
+			# size = int(torch.rand([])**self.cut_pow * (max_size - min_size) + min_size)
+			# offsetx = torch.randint(0, sideX - size + 1, ())
+			# offsety = torch.randint(0, sideY - size + 1, ())
+			# cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
+			# cutouts.append(resample(cutout, (self.cut_size, self.cut_size)))
+			
+			# cutout = transforms.Resize(size=(self.cut_size, self.cut_size))(input)
+			
+			cutout = (self.av_pool(input) + self.max_pool(input))/2
+			cutouts.append(cutout)
+		batch = self.augs(torch.cat(cutouts, dim=0))
+		if self.noise_fac:
+				facs = batch.new_empty([self.cutn, 1, 1, 1]).uniform_(0, self.noise_fac)
+				batch = batch + facs * torch.randn_like(batch)
+		return batch
+
+	# def __init__(self, cut_size, cutn, cut_pow=1.):
+	# 		super().__init__()
+	# 		self.cut_size = cut_size
+	# 		self.cutn = cutn
+	# 		self.cut_pow = cut_pow
+
+	# 		self.augs = nn.Sequential(
+	# 			# K.RandomHorizontalFlip(p=0.5),
+	# 			# K.RandomVerticalFlip(p=0.5),
+	# 			# K.RandomSolarize(0.01, 0.01, p=0.7),
+	# 			# K.RandomSharpness(0.3,p=0.4),
+	# 			# K.RandomResizedCrop(size=(self.cut_size,self.cut_size), scale=(0.1,1),  ratio=(0.75,1.333), cropping_mode='resample', p=0.5),
+	# 			# K.RandomCrop(size=(self.cut_size,self.cut_size), p=0.5),
+	# 			K.RandomAffine(degrees=15, translate=0.1, p=0.7, padding_mode='border'),
+	# 			K.RandomPerspective(0.7,p=0.7),
+	# 			K.ColorJitter(hue=0.1, saturation=0.1, p=0.7),
+	# 			K.RandomErasing((.1, .4), (.3, 1/.3), same_on_batch=True, p=0.7),
+	# 		)
+	# 		self.noise_fac = 0.1
+	# 		self.av_pool = nn.AdaptiveAvgPool2d((self.cut_size, self.cut_size))
+	# 		self.max_pool = nn.AdaptiveMaxPool2d((self.cut_size, self.cut_size))
+	
+	# def forward(self, input):
+	# 	# sideY, sideX = input.shape[2:4]
+	# 	# max_size = min(sideX, sideY)
+	# 	# min_size = min(sideX, sideY, self.cut_size)
+	# 	cutouts = []
+
+	# 	for _ in range(self.cutn):
+	# 		# size = int(torch.rand([])**self.cut_pow * (max_size - min_size) + min_size)
+	# 		# offsetx = torch.randint(0, sideX - size + 1, ())
+	# 		# offsety = torch.randint(0, sideY - size + 1, ())
+	# 		# cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
+	# 		# cutouts.append(resample(cutout, (self.cut_size, self.cut_size)))
+			
+	# 		# cutout = transforms.Resize(size=(self.cut_size, self.cut_size))(input)
+			
+	# 		cutout = (self.av_pool(input) + self.max_pool(input))/2
+	# 		cutouts.append(cutout)
+	# 	batch = self.augs(torch.cat(cutouts, dim=0))
+	# 	if self.noise_fac:
+	# 			facs = batch.new_empty([self.cutn, 1, 1, 1]).uniform_(0, self.noise_fac)
+	# 			batch = batch + facs * torch.randn_like(batch)
+	# 	return batch
 
 class ResnetBlock(nn.Module):
 	in_channels: int
